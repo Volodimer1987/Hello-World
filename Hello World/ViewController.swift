e39048c694b41222a3f60c5bd0944916178e9364
Vladimir <kynicin87@mail.ru>
2020-07-09 18:35:18 +0300

    last

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -5,9 +5,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+  
 
 }
 
