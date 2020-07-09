55a615e3eb9d1896588fb5ad10bec0bc6ea3f79c
Vladimir <kynicin87@mail.ru>
2020-07-10 00:14:05 +0300

    конечный продукт !)) конец урока

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -2,12 +2,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var labelHellowWorld: UILabel!
+    @IBOutlet var labelOfButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        labelHellowWorld.isHidden = true
+        labelOfButton.layer.cornerRadius = 10
     }
-  
+    
+    @IBAction func pressButtonShowText() {
+        
+        if labelHellowWorld.isHidden {
+            labelHellowWorld.isHidden = false
+            labelOfButton.setTitle("clear text", for: .normal)
+            
+        } else {
+            
+            labelHellowWorld.isHidden = true
+            labelOfButton.setTitle("show text", for: .normal)
+        }
+    }
+    
 
 }
-
